4207cc64dc30215c63725a697d61c41953b83641
JohanVillard <styx.locke@gmail.com>
2024-08-16 16:43:11 +0200

    Splits the tokenize function into 2

--- tokenize.c
+++ tokenize.c
@@ -6,9 +6,28 @@
  * @tokens: result (input into tokens)
  * @token: each token
  * @delim: token delimiter
+ * @token_count: is the number of tokens in the string
+ *
+ * Return: an array of pointers pointin arrays
  */
-void tokenize_input(char *ui, char **tokens, char *token, char *delim)
+char **tokenize_input(char *ui, char **tokens, char *token,
+					char *delim, int token_count)
 {
+	int i = 0; /* Index for tokens */
+
+	/* Allocate memory for tokens + 1 to NULL terminate the array */
+	tokens = malloc(sizeof(char *) * (token_count + 1));
+	if (tokens == NULL)
+	{
+		free(ui);
+		ui = NULL;
+		error_handling("malloc", EXIT_FAILURE);
+	}
+
+	/* Tokenize user input */
+	token = strtok(ui, delim);
+	while (token)
+	{
 		tokens[i] = strdup(token);
 		if (tokens[i] == NULL)
 		{
@@ -20,6 +39,12 @@ void tokenize_input(char *ui, char **tokens, char *token, char *delim)
 		}
 		i++;
 		token = strtok(NULL, delim);
+	}
+
+	/* Null terminate the array of pointer */
+	tokens[i] = NULL;
+
+	return (tokens);
 }
 
 /**
@@ -34,7 +59,6 @@ char **tokenize(char *user_input)
 	char *token = NULL; /* Word cutting from user_input by strtok */
 	char *delimiter = " \n";  /* Delimits the tokens */
 	int token_count = 0;	/* Stores the number of tokens in unsr_input */
-	int i = 0; /* Index for tokens */
 	char *cp_input = NULL;	/* Create a copy to count number of tokens */
 
 	/* Copy user_ input and error hanling */
@@ -56,21 +80,7 @@ char **tokenize(char *user_input)
 	free(cp_input); /* Free the copy */
 	cp_input = NULL;
 
-	/* Allocate memory for tokens + 1 to NULL terminate the array */
-	tokens = malloc(sizeof(char *) * (token_count + 1));
-	if (tokens == NULL)
-	{
-		free(user_input);
-		user_input = NULL;
-		error_handling("malloc", EXIT_FAILURE);
-	}
-
-	/* Tokenize user input */
-	token = strtok(user_input, delimiter);
-	while (token)
-		tokenize_input(user_input, tokens, token, delimiter);
-	/* Null terminate the array of pointer */
-	tokens[i] = NULL;
+	tokens = tokenize_input(user_input, tokens, token, delimiter, token_count);
 
 	return (tokens);
 }
