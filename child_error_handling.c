2123df791c48601df10ce46f326a6494dbe4b5d7
JohanVillard <styx.locke@gmail.com>
2024-08-18 18:12:19 +0200

    Change short, parameters description, and and add a more comprehensive description

--- child_error_handling.c
+++ child_error_handling.c
@@ -1,12 +1,15 @@
 #include "simple_shell.h"
 
 /**
- * child_error_handling - is used to handle
- * execution failures of a child process
- * @msg: is a string stroring the error message
- * @exit_code: to indicate
+ * child_error_handling - handles errors for a child process
+ * @msg: is a string containing the error message to be printed
+ * @exit_code: for the child process
  *
- * Return : Always nothing
+ * Description: This function prints an error message using `perror`
+ * and exits the child process with the specified exit code. It uses
+ * _exit that avoid interference with the parent like unnecessary clean-up.
+ *
+ * Return : Always void
  */
 void child_error_handling(char *msg, int exit_code)
 {
