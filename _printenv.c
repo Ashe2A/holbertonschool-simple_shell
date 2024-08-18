d6cc9b6f93666b3fa374c2201976140a1d41ff88
JohanVillard <styx.locke@gmail.com>
2024-08-18 17:28:47 +0200

    Delete copy of environ, add and improve description of the function

--- _printenv.c
+++ _printenv.c
@@ -1,16 +1,24 @@
 #include "simple_shell.h"
 
 /**
- * _printenv - print the environments variables
- * @user_input: unused
- * @tokens: unsused
+ * _printenv - print the environment variables
+ * @user_input: unused parameter
+ * @tokens: unsused parameter
  *
- * Return: Always nothing
+ * Description: This function prints all the environment variables
+ * available in the current shell session. The function does not
+ * return any value.
+ *
+ * Return: Always void
  */
 void _printenv(char *user_input __attribute__((unused)),
 			char **tokens __attribute__((unused)))
 {
-	char **cpy_environ = environ;
+	int i = 0;
 
-	printf("%s\n", *cpy_environ);
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
 }
