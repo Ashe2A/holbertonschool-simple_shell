42e6c6ffd994fc12cf9af0e7a5a93848ada3519b
JohanVillard <styx.locke@gmail.com>
2024-08-18 17:15:23 +0200

    Delete env arg, delete copy of environ, while condition -1 to 1, change name variable read into read_bytes and improve comments

--- simple_shell.c
+++ simple_shell.c
@@ -1,32 +1,33 @@
 #include "simple_shell.h"
 
 /**
-	* main - entry point
-	* @argc: is the number of arguments
-	* @argv: is an array of pointers pointing arrays of string
-	* @env: environment variables
+	* main - entry point of the program
+	* @argc: is the number of command-line argument
+	* @argv: is an array of pointers pointing arrays of strings
+	* storing the command-line arguments
 	*
-	* Return: (0)
+	* Return: Always returns 0 (success)
 	*/
-int main(int argc __attribute__((unused)), char **argv, char **env)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	int is_interactive = 0; /* 0 is off, 1 is on on */
-	int read = 0; /* Is the number of bytes read by getline */
-	char *user_input = NULL; /* Stored by getline */
+	int bytes_read = 0; /* Is the number of bytes bytes_read by getline */
+	char *user_input = NULL; /* Buffer to store user input */
 	size_t n = 0;	/* is the number of bytes allocated to user_input by getline */
-	char **cpy_env = env;	/* is environment to export in the new shell */
 
-	while (-1)
+	while (1)	/* Infinite loop for shell */
 	{
 		/* Display prompt and switch interactive mode */
 		is_interactive = initialize_mode_and_prompt();
 
-		/* Waits and stores the user's input and manages CTRL + D*/
-		read = getline(&user_input, &n, stdin);
-		handle_eof_cleanup(read, user_input);
+		/* Wait and store the user's input; manages CTRL + D */
+		bytes_read = getline(&user_input, &n, stdin);
+		handle_eof_cleanup(bytes_read, user_input);
 
-		/* Parses and execute command */
-		handle_user_command(user_input, read, cpy_env, is_interactive, argv);
+		/* Parse and execute command */
+		handle_user_command(user_input, bytes_read, environ, is_interactive, argv);
 	}
+
+	/* Cleanup memory */
 	free(user_input);
 }
