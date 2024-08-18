a7a9ffef1ddcdea2212546cba2d55e2800adb666
JohanVillard <styx.locke@gmail.com>
2024-08-18 17:37:08 +0200

    Remove bad logic. Now return 1 when a built function is found or return 0 if not and improve comments

--- check_and_run_builtin.c
+++ check_and_run_builtin.c
@@ -2,36 +2,36 @@
 
 /**
  * check_and_run_builtin - call the built-in function
- * of the corresponding token if exists
+ * corresponding to the command if it exists.
  * @user_input: is an array of character strings
  * containing the raw data entered by the user
  * @tokens: is an array of pointers to character strings
  * containing the command and arguments given by the user
  *
- * Return: 0 for no built-in mode, 1 for built-in
+ * Return: 1 for built-in mode, 0 otherwise
  */
 int check_and_run_builtin(char *user_input, char **tokens)
 {
+	/* The structure storing the command and its built-in function */
 	built_in_t built_in_list[] = {
 		{"env", _printenv},
 		{"exit", _exit_function},
 		{NULL, NULL}
 	};
 	int i = 0;
-	int built_in_mode = 0;
 
-	/* Compare user's input with cmd from structure an f run function if find */
+	/* Compare user's input with cmd from structure and run function if found */
 	while (built_in_list[i].cmd != NULL)
 	{
 		if (strcmp(tokens[0], built_in_list[i].cmd) == 0)
+		{
 			built_in_list[i].f(user_input, tokens);
-
+			/* Built-in mode on */
+			return (1);
+		}
 		i++;
 	}
 
-	/* If command is not found, no built-in mode */
-	if (built_in_list[i].cmd != NULL)
-		built_in_mode = 1;
-
-	return (built_in_mode);
+	/* Built-in mode on */
+	return (0);
 }
